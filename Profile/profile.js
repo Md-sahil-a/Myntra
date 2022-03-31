ad4b71ff85da05b299f2059d8f9c1d080ad4f6f0
Aadarsh Mukati <aadarsh11433@gmail.com>
2022-03-31 11:23:27 +0530

    profile page added

---
+++ Profile/profile.js
@@ -0,0 +1,44 @@
+document.querySelector("button").addEventListener("click",loginfun)
+
+ var userdata =JSON.parse(localStorage.getItem("userarr")) ||[];
+console.log(userdata)
+
+function loginfun(){
+    var in_mob =document.querySelector("#mob").value;
+  
+    for(var a=userdata.length-1; a>=0 ;a--)
+  {
+    if (in_mob ==""){
+        alert("enter mobile to login")
+        break;
+       }
+    
+     if(in_mob == userdata[a].mob){
+           if( checkpass(a)){
+             console.log(checkpass(a))
+             alert("login successful")
+
+              window.location.href ="menHomePage.html";
+              break;
+           }else{
+             alert("wrong password")
+             document.querySelector("#pass").value="";
+           }
+      }
+      else if(a==userdata.length-1){
+        
+        alert("you dont have account sign up first")
+    
+        window.location.href="signup.html"
+      }
+
+  }
+}
+
+function checkpass(a){
+
+  var in_pass =document.querySelector("#pass").value;
+
+   return ( userdata[a].password == in_pass)
+     
+}
