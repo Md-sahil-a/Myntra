616b35cf64c423d765f3a15229f7a49c65a31ee2
Md sahil amaan <77974484+sahilamaan@users.noreply.github.com>
2022-04-02 10:06:53 +0530

    womens data

---
+++ womensdata/style&js/womens.js
@@ -0,0 +1,434 @@
+var womensData = [{
+        image_url: "https://bit.ly/3uHGEnN",
+        name: "Yoke design kurta",
+        para: "Women Pink Self Design Kurta with Trousers & Dupatta",
+        price: "Rs. 791",
+        strikedoffprice: "Rs. 3298",
+        offer: "(76% OFF)",
+        wishList: "WISHLIST ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3uLGTyi",
+        name: "Yoke design kurta",
+        para: "Women yellow Self Design Kurta with Trousers & Dupatta",
+        price: "Rs. 758",
+        strikedoffprice: "Rs. 3299",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3Dldqit",
+        name: "Yoke design kuta",
+        para: "Green Ethnic Motifs Pure Cotton Printed Fit and Flare Dress",
+        price: "Rs. 749",
+        strikedoffprice: "Rs. 1299",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST  ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3iNMWwx",
+        name: "Chevron",
+        para: "Black & Grey Chevron Geometric Printed Wrap Dress",
+        price: "Rs. 1199",
+        strikedoffprice: "Rs. 2599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3IQuE8v",
+        name: "Yoke design kurta",
+        para: "Women Olive Green Ethnic Motifs Yoke Design Kurta with Palazzos & With Dupatta",
+        price: "Rs. 879",
+        strikedoffprice: "Rs. 2499",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3IRLbJa",
+        name: "MFB",
+        para: "Women Maroon Analogue Watch MFB-PN-SNT-C30-1",
+        price: "Rs. 674",
+        strikedoffprice: "Rs. 1499",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST ♡",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wYoUqM",
+        name: "Libas",
+        para: "Women Black Pure Cotton Ethnic Motifs Printed Kurta with Palazzos & Dupatta",
+        price: "Rs. 1035",
+        strikedoffprice: "Rs. 2799",
+        offer: "(63% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wXtNAx",
+        name: "Nayo",
+        para: "Women Red Floral Printed Kurta With Trouser & Dupatta",
+        price: "Rs. 1479",
+        strikedoffprice: "Rs. 3699",
+        offer: "(60% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/37aGnRW",
+        name: "Yash gallery",
+        para: "Women Blue & White Printed A-Line Kurta",
+        price: "Rs. 805",
+        strikedoffprice: "Rs. 1299",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wUyQli",
+        name: "Ahalyaa",
+        para: "Women Charming Maroon Ethnic Motifs Bling It On Dress",
+        price: "Rs. 489",
+        strikedoffprice: "Rs. 999",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3uMmnxi",
+        name: "Sangria",
+        para: "Black& Mustard Yellow Pure Cotton Ethnic Motifs Printed A-Line Dress",
+        price: "Rs. 799",
+        strikedoffprice: "Rs. 1199",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3IZknGX",
+        name: "Juniper",
+        para: "Women Black & Gold-Toned Printed Flared Palazzos",
+        price: "Rs. 883",
+        strikedoffprice: "Rs. 1699",
+        offer: "(43% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/35xpQr3",
+        name: "W",
+        para: "Women Lime Green & White Striped Flared Palazzos",
+        price: "Rs. 594",
+        strikedoffprice: "Rs. 1699",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3iUMb4X",
+        name: "Vishudh",
+        para: "Women Off-White & Gold-Coloured Printed Flared Palazzos",
+        price: "Rs. 499",
+        strikedoffprice: "Rs. 1249",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/36I0Myd",
+        name: "Saadgi",
+        para: "Women White Chikankari Embroidered Flared Palazzos",
+        price: "Rs. 997",
+        strikedoffprice: "Rs. 2850",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3DwTnO0",
+        name: "Anouk",
+        para: "Women Blue Ethnic Motifs Embroidered Knitted Cropped Ethnic Palazzos",
+        price: "Rs. 554",
+        strikedoffprice: "Rs. 1499",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3v7U4K7",
+        name: "Shubhkala",
+        para: "Maroon Embroidered Semi-Stitched Lehenga & Unstitched Blouse with Dupatta",
+        price: "Rs. 3999",
+        strikedoffprice: "Rs. 10000",
+        offer: "(Rs. 60001 OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3iXV6Cw",
+        name: "6Y COLLECTIVE",
+        para: "Beige & Golden Embellished Sequinned Semi-Stitched Lehenga Set",
+        price: "Rs. 5100",
+        strikedoffprice: "Rs. 6800",
+        offer: "(25% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3DvsXfI",
+        name: "LOOKNBOOK ART",
+        para: "Black Embellished Sequinned Semi-Stitched Lehenga & Unstitched Blouse With Dupatta",
+        price: "Rs. 2457",
+        strikedoffprice: "Rs. 3330",
+        offer: "(25% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wYy1Im",
+        name: "Kvsfab",
+        para: "Pink Embellished Sequinned Semi-Stitched Lehenga & Unstitched Blouse With Dupatta",
+        price: "Rs. 2319",
+        strikedoffprice: "Rs. 7999",
+        offer: "(71% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/35wbrLE",
+        name: "rubans LUXURY",
+        para: "24K Gold-Plated White & Red Stone-Studded Beaded Jewellery Set",
+        price: "Rs. 2080",
+        strikedoffprice: "Rs. 6500",
+        offer: "(20% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3DvtdeG",
+        name: "Rubans",
+        para: "Gold-Plated Pink & Green Enamelled Beaded Kundan-Studded Handcrafted Jewellery Set",
+        price: "Rs. 4448",
+        strikedoffprice: "Rs. 6390",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wVpBRV",
+        name: "Rubans",
+        para: "Gold-Toned & Pink CZ Stone-Studded Jewellery Set",
+        price: "Rs. 539",
+        strikedoffprice: "Rs. 2998",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wUhtRH",
+        name: "Priyaasi",
+        para: "Green & Pink Gold-Plated Kundan-Studded Handcrafted Jewellery Set",
+        price: "Rs. 894",
+        strikedoffprice: "Rs. 2360",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3Dv4h6W",
+        name: "KALINI",
+        para: "Mustard Yellow & Red Bandhani Print Saree",
+        price: "Rs. 1799",
+        strikedoffprice: "Rs. 2199",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3NGRzXv",
+        name: "Sangria",
+        para: "Navy Blue Solid Saree",
+        price: "Rs. 489",
+        strikedoffprice: "Rs. 999",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3DBhHP0",
+        name: "Sangria",
+        para: "Burgundy Sequinned Satin Saree",
+        price: "Rs. 549",
+        strikedoffprice: "Rs. 999",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/35vC57k",
+        name: "INDDUs",
+        para: "Teal Blue Solid Accordian Pleat Ruffles Saree with Embroidered Blouse",
+        price: "Rs. 599",
+        strikedoffprice: "Rs. 1999",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3uGJsBy",
+        name: "Libas",
+        para: "Black Embroidered Ethnic Motifs A-Line Dress",
+        price: "Rs. 159",
+        strikedoffprice: "Rs. 199",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3v7Vmor",
+        name: "Anekaant",
+        para: "https://bit.ly/3v7Vmor",
+        price: "Rs. 879",
+        strikedoffprice: "Rs. 1699",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wWha8K",
+        name: "W",
+        para: "Women Beige Printed Shirt Dress",
+        price: "Rs. 1049",
+        strikedoffprice: "Rs. 2599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/37bUbfj",
+        name: "Annekant",
+        para: "Beige & Gold-Toned Floral Embroidered & Sequinned Box Clutches",
+        price: "Rs. 749",
+        strikedoffprice: "Rs. 1599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3iX4lTz",
+        name: "Zaveri Pearls",
+        para: "Gold-Plated Clustered Pearls Cuff Bracelet",
+        price: "Rs. 554",
+        strikedoffprice: "Rs. 1599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3wXwlP7",
+        name: "zeaveripearls",
+        para: "Gold-Toned & White Kundan & Pearl Studded Ring Bracelet",
+        price: "Rs. 949",
+        strikedoffprice: "Rs. 2599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://bit.ly/3NEADRn",
+        name: "Teejh",
+        para: "Women Silver-Toned Oxidised Cuff Bracelet",
+        price: "Rs. 623",
+        strikedoffprice: "Rs. 2599",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+    {
+        image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11715910/2020/12/9/9442c1af-94fd-4c60-b9c8-044ed39535ec1607497230477-Rubans-Women-Silver-Plated-Oxidised-Elasticated-Bracelet-485-1.jpg",
+        name: "Rubans",
+        para: "Women Silver-Plated Oxidised Elasticated Bracelet",
+        price: "Rs. 899",
+        strikedoffprice: "Rs. 2999",
+        offer: "(65% OFF)",
+        wishList: "WISHLIST",
+        addToBag: "ADD TO BAG",
+    },
+];
+
+var wishList = JSON.parse(localStorage.getItem("wishListObj")) || [];
+
+var bagList = JSON.parse(localStorage.getItem("BagListObj")) || [];
+
+womensData.map(function(element) {
+    var box = document.createElement("div");
+    box.style.cursor = "pointer";
+
+    var img = document.createElement("img");
+    img.src = element.image_url;
+
+    var contentBox = document.createElement("div");
+    contentBox.setAttribute("class", "contentBox");
+
+    var name = document.createElement("h4");
+    name.textContent = element.name;
+
+    var para = document.createElement("p");
+    para.textContent = element.para;
+
+    var mix = document.createElement("div");
+    mix.setAttribute("class", "mixbox");
+
+    var price = document.createElement("p");
+    price.textContent = element.price;
+
+    var strikeprice = document.createElement("p");
+    strikeprice.textContent = element.strikedoffprice;
+    strikeprice.setAttribute("class", "strikep");
+
+    var offer = document.createElement("p");
+    offer.textContent = element.offer;
+    offer.setAttribute("class", "offerp");
+
+    mix.append(price, strikeprice, offer);
+
+    var wishList = document.createElement("p");
+    wishList.setAttribute("class", "wishListp");
+    wishList.textContent = element.wishList;
+    wishList.style.cursor = "pointer";
+
+    wishList.addEventListener("click", function() {
+        addToWishlist(element);
+        wishList.style.color = "green";
+        wishList.innerText = "GO TO WISHLIST";
+    });
+
+    var addToBag = document.createElement("p");
+    addToBag.setAttribute("class", "addToBagp");
+    addToBag.textContent = element.addToBag;
+    addToBag.style.cursor = "pointer";
+
+    addToBag.addEventListener("click", function() {
+        addToBaglist(element);
+        addToBag.innerText = "GO TO BAG >";
+    });
+
+    contentBox.append(name, para, mix, wishList, addToBag);
+    box.append(img, contentBox);
+
+    document.querySelector("#container").append(box);
+});
+
+function addToWishlist(element) {
+    wishList.push(element);
+    localStorage.setItem("wishListObj", JSON.stringify(wishList));
+}
+
+function addToBaglist(element) {
+    bagList.push(element);
+    localStorage.setItem("BagListObj", JSON.stringify(bagList));
+}
